d637f4826a67487e7b79a183d80e26307b1d14d3
MS_D_rei <46997561+MS-D-rei@users.noreply.github.com>
2021-12-07 18:10:11 +0900

    implement basic login

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -30,18 +30,20 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
       email: @user.email,
       password: 'invalid'
     } }
+    assert_not is_logged_in?
     assert_template 'sessions/new'
     assert_not flash.empty?
     get root_path
     assert flash.empty?
   end
 
-  test 'login with valid information' do
+  test 'login with valid info followed by logout' do
     get login_path
     post login_path, params: { session: {
       email: @user.email,
       password: 'password'
       } }
+    assert is_logged_in?
     # check redirect destination is @user or not
     assert_redirected_to @user
     # move to the redirect destination
@@ -53,5 +55,11 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     # check the page shows logout and profile
     assert_select 'a[href=?]', logout_path
     assert_select 'a[href=?]', user_path(@user)
+    delete logout_path
+    assert_redirected_to root_path
+    follow_redirect!
+    assert_select 'a[href=?]', login_path
+    assert_select 'a[href=?]', logout_path, count: 0
+    assert_select 'a[href=?]', user_path(@user), count: 0
   end
 end
