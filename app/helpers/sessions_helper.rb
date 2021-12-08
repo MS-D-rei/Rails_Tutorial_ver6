554eeede9ff5bb9b89b23effbfc29f53a8cf39dc
MS_D_rei <46997561+MS-D-rei@users.noreply.github.com>
2021-12-08 13:13:42 +0900

    add delete cookies and logout

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -25,7 +25,15 @@ module SessionsHelper
     !current_user.nil?
   end
 
+  # delete remember_digest, remember_token, user_id
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
+
   def log_out
+    forget(current_user)
     session.delete(:user_id)
     @current_user = nil
   end
