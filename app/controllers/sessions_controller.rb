902436e7cbbb5a0318d3977ddfdef7f751179456
MS_D_rei <46997561+MS-D-rei@users.noreply.github.com>
2021-12-08 13:05:51 +0900

    add cookies login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,7 @@ class SessionsController < ApplicationController
     if user&.authenticate(params[:session][:password])
       # user && user.authenticate
       log_in user
+      remember user
       redirect_to user
     else
       flash.now[:danger] = 'Invalid email/password combination'
