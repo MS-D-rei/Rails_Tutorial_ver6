efb7452e095e6e6f5a4be9dac7c4db3c886bfb94
MS_D_rei <46997561+MS-D-rei@users.noreply.github.com>
2021-12-13 13:51:47 +0900

    add activation_token, digest

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,7 +15,9 @@ User.create!(
   email: 'example@railstutorial.org',
   password: 'foobar',
   password_confirmation: 'foobar',
-  admin: true
+  admin: true,
+  activated: true,
+  activated_at: Time.zone.now
 )
 
 # addtional users
@@ -27,6 +29,8 @@ User.create!(
     name: name,
     email: email,
     password: password,
-    password_confirmation: password
+    password_confirmation: password,
+    activated: true,
+    activated_at: Time.zone.now
   )
 end
